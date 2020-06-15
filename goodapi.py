c173fb69c69e9ac4c12dd6b312031142fc5b9f19
gsoftinccode <goddietshetu@gmail.com>
2020-06-15 22:32:50 +0200

    updated somewhere

--- goodapi.py
+++ goodapi.py
@@ -15,5 +15,14 @@ def process():
         return jsonify(book)
     except Exception as e:
         return "Image processing could not complete:\n"+ str(e)
+@app.route('/home', methods=['POST','GET'])  
+def process():
+    try: 
+        
+
+        return jsonify({"greetongs": "Greetings every one over there"})
+    except Exception as e:
+
+        return "There was an error somewhere:\n"+ str(e)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
