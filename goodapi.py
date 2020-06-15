858f8b73d8453249e5f100afdeb6fbe035ad14fd
gsoftinccode <goddietshetu@gmail.com>
2020-06-15 22:18:54 +0200

    committed to the local

---
+++ goodapi.py
@@ -0,0 +1,19 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# -------------------------------------------------------
+
+
+@app.route('/process', methods=['POST','GET'])  
+def process():
+    try: 
+        book = {"id":376236727, "name":"Computer Science Essentials","author":"Hari Prasaad","year":2003}
+
+        return jsonify(book)
+    except Exception as e:
+        return "Image processing could not complete:\n"+ str(e)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
